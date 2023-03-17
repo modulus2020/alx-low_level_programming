a167f9296b5511636eb9bd94966d1d0d802becd5
Joshua Jackson (Yuppiefab) <masterjack612@gmail.com>
2023-03-16 19:34:51 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry into this program that uses the printf function
+ *
+ * Return: 0 (Success)
+ *
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
