020175c368e376d0e534b65ac3b5ac0bb0eb4e2a
Joshua Jackson (Yuppiefab) <masterjack612@gmail.com>
2023-03-18 09:44:04 -0700

    3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Program that prints the alphabets a-z
+ * Return: always (Success)
+ *
+ */
+
+int main(void)
+{
+	char c, d;
+
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar(d);
+	putchar('\n');
+	return (0);
+}
