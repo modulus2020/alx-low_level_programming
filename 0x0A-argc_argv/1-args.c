e3cc982b0a3f994ea5e68bfb7aaad8d6577e6190
Joshua Jackson (Yuppiefab) <masterjack612@gmail.com>
2023-04-09 20:25:47 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - A program that prints the number of arguments
+ * @argc: The number of arguments in the main function
+ * @argv: An array of arguments in main
+ *
+ * Return: 0 (Always)
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+(void) argv; /* To ignore unused argument*/
+printf("%d\n", argc);
+return (0);
+}
