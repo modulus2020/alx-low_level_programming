b1a350967a2df1a485b67f26cc4584bc46c05dd6
Joshua Jackson (Yuppiefab) <masterjack612@gmail.com>
2023-04-11 21:22:06 -0700

    2-str_concat.c

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * str_concat - a function that concatenates two strings
+ * @str1: the first string variable pointer
+ * @str2: the second string variable pointer
+ *
+ * return: 0 Always
+ */
+int
