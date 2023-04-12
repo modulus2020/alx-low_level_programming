72869455dc63cfd0f9299861a9cfbbf0133b4270
Joshua Jackson (Yuppiefab) <masterjack612@gmail.com>
2023-04-11 21:32:23 -0700

    3-alloc_grid.c

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - a function that returns a pointer to a
+ * 2 dimensional array of integers
+ * @width: the number of columns
+ * @height: the number of rows
+ *
+ * return: 0 Always
+ */
+ 
