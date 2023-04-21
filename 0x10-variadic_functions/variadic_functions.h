e155509c2f42ff3b459e6e056402c4a640ff8414
Joshua Jackson (Yuppiefab) <masterjack612@gmail.com>
2023-04-20 20:31:01 -0700

    a function that returns the sum of all its parameters

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,7 +1,7 @@
 #ifndef _HEADER_
 #define _HEADER_
 
-Prototype: int sum_them_all(const unsigned int n, ...);
+int sum_them_all(const unsigned int n, ...);
 
 void print_numbers(const char *separator, const unsigned int n, ...);
 
