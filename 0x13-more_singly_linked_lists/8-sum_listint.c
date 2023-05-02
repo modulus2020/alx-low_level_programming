75ad6902aae7c25abd217c9bfe820db795f60fcf
Joshua Jackson (Yuppiefab) <masterjack612@gmail.com>
2023-05-02 15:49:39 -0700

    8-sum_listint.c

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * sum_listint - function that sums all the data (n) of
+ * a linked list.
+ * @head: head of a list.
+ *
+ * Return: sum of all the data (n).
+ */
+int sum_listint(listint_t *head)
+{
+	int add;
+
+	add = 0;
+	while (head != NULL)
+	{
+		add += head->n;
+		head = head->next;
+	}
+
+	return (add);
+}
